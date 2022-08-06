ace060957144c2322c36584c08b81e3e724d6470
ebrodsky <34733559+ebrodsky@users.noreply.github.com>
2022-08-05 23:28:47 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ mod engine;
 fn main() {
     let mut engine = engine::PaymentEngine::new();
     let _res = engine.read_csv();
-    //engine.print_transactions();
-    //engine.print_entries();
     let _res = engine.process_transactions();
     engine.output_accounts();
 }
